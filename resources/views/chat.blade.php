5a263ee43567b0d5417532d6f76e15562efc0c0d
Esdras <Exdraz_Lpz@outlook.com>
2019-01-15 00:50:03 -0600

    chat funciona solo en la visa home

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,5 +1,7 @@
-  <script src="{{ asset('js/app.js') }}" defer></script>
- <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+ 
+@extends('layouts.app')
+
+@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-sm-12">
@@ -13,3 +15,4 @@
         </div>
     </div>
 </div>
+@endsection
